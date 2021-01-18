c6692df09edc521cf7b417175b8fe69ad53af377
Daniel Kool <kool7d@gmail.com>
2021-01-18 03:25:52 -0600

    first.

--- src/DictionaryIndexing.jl
+++ src/DictionaryIndexing.jl
@@ -1,5 +1,29 @@
 module DictionaryIndexing
 
-# Write your package code here.
+(D::Dict)(i::Int) = Dict([keys(D)...][i] => [values(D)...][i])
+(D::OrderedDict)(i::Int) = OrderedDict([keys(D)...][i] => [values(D)...][i])
+(D::Dict)(is::AbstractVector{Int}) = Dict([([keys(D)...][i],[values(D)...][i]) for i in is])
+(D::OrderedDict)(is::AbstractVector{Int}) = OrderedDict([([keys(D)...][i],[values(D)...][i]) for i in is])
+(D::Dict)(is::Int...) = D([is...])
+(D::OrderedDict)(is::Int...) = D([is...])
+(D::Dict)(is::AbstractRange{Int}) = D([is...])
+(D::OrderedDict)(is::AbstractRange{Int}) = D([is...])
+(D::Dict)(is::AbstractRange{Int}...) = D([(is...)...])
+(D::OrderedDict)(is::AbstractRange{Int}...) = D([(is...)...])
+function (D::AbstractDict)(is...)
+    indices = []
+    for i in is
+        if typeof(i) <: Union{AbstractRange{Int},AbstractArray{Int}}
+            for j in i
+                push!(indices, j)
+            end
+        elseif typeof(i) <: Int
+            push!(indices, i)
+        else
+            error("could not index this, arguments must be Ints, ranges of Ints, arrays of Ints, or a combo of those")
+        end
+    end
+    return D(indices...)
+end
 
 end
