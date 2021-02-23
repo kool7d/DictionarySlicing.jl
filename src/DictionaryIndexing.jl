bafd389beba45cd54f9729e81d3261dfed3cdcf7
kool7d <kool7d@gmail.com>
2021-02-23 13:22:52 -0600

    macro activation

--- src/DictionaryIndexing.jl
+++ src/DictionaryIndexing.jl
@@ -2,31 +2,32 @@ module DictionaryIndexing
 
 using OrderedCollections
 
-(D::Dict)(i::Int) = Dict([keys(D)...][i] => [values(D)...][i])
-(D::OrderedDict)(i::Int) = OrderedDict([keys(D)...][i] => [values(D)...][i])
-(D::Dict)(is::AbstractVector{Int}) = Dict([([keys(D)...][i],[values(D)...][i]) for i in is])
-(D::OrderedDict)(is::AbstractVector{Int}) = OrderedDict([([keys(D)...][i],[values(D)...][i]) for i in is])
-(D::Dict)(is::Int...) = D([is...])
-(D::OrderedDict)(is::Int...) = D([is...])
-(D::Dict)(is::AbstractRange{Int}) = D([is...])
-(D::OrderedDict)(is::AbstractRange{Int}) = D([is...])
-(D::Dict)(is::AbstractRange{Int}...) = D([(is...)...])
-(D::OrderedDict)(is::AbstractRange{Int}...) = D([(is...)...])
+macro idxdict()
+    quote
+        (D::AbstractDict)(i::Int) = OrderedDict([keys(D)...][i] => [values(D)...][i])
+        (D::AbstractDict)(is::AbstractVector{Int}) = OrderedDict([([keys(D)...][i],[values(D)...][i]) for i in is])
+        (D::AbstractDict)(is::Int...) = D([is...])
+        (D::AbstractDict)(is::AbstractRange{Int}) = D([is...])
+        (D::AbstractDict)(is::AbstractRange{Int}...) = D([(is...)...])
 
-function (D::AbstractDict)(is...)
-    indices = []
-    for i in is
-        if typeof(i) <: Union{AbstractRange{Int},AbstractArray{Int}}
-            for j in i
-                push!(indices, j)
+        function (D::AbstractDict)(is...)
+            indices = []
+            for i in is
+                if typeof(i) <: Union{AbstractRange{Int},AbstractArray{Int}}
+                    for j in i
+                        push!(indices, j)
+                    end
+                elseif typeof(i) <: Int
+                    push!(indices, i)
+                elseif typeof(i) <: AbstractArray
+                    return D(i...)
+                else
+                    error("could not index this, arguments must be Ints, ranges of Ints, arrays of Ints, or a combo of those")
+                end
             end
-        elseif typeof(i) <: Int
-            push!(indices, i)
-        else
-            error("could not index this, arguments must be Ints, ranges of Ints, arrays of Ints, or a combo of those")
+            return D(indices...)
         end
     end
-    return D(indices...)
 end
 
 end
