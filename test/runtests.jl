058e564c17ab7e13b3af40d8008ab1c1771aeecd
kool7d <kool7d@gmail.com>
2021-07-16 01:51:53 -0500

    tst1

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,19 +2,5 @@ using DictionarySlicing
 using Test
 
 @testset "DictionarySlicing.jl" begin
-    od = OrderedDict(
-             :Apl => "apple",
-     		 :Brc => "birch",
-     		 :Cnd => "candle",
-     		 :Drn => "dragon",
-     		 :Exp => "expensive",
-     		 :Frg => "forage",
-     		 :Gra => "grain",
-     		 :Hlt => "health",
-     		 :Irn => "irony",
-     		 :Jak => "jackal" 
-    )
-    @test sliced(od,4:6) == OrderedDict{Symbol,String}( :Drn => "dragon",
-          		                                        :Exp => "expensive",
-          		                                        :Frg => "forage")
+
 end
