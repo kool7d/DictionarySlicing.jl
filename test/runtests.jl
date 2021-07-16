8f58de835a2f320ec7bbd0302c7d4ad73c36afeb
Dan Kool <kool7d@gmail.com>
2021-07-16 01:13:40 -0500

    Update runtests.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,17 +2,19 @@ using DictionarySlicing
 using Test
 
 @testset "DictionarySlicing.jl" begin
-    # dd = OrderedDict(:Apl => "apple",
-    # 		 :Brc => "birch",
-    # 		 :Cnd => "candle",
-    # 		 :Drn => "dragon",
-    # 		 :Exp => "expensive",
-    # 		 :Frg => "forage",
-    # 		 :Gra => "grain",
-    # 		 :Hlt => "health",
-    # 		 :Irn => "irony",
-    # 		 :Jak => "jackal" )     # length is 10
-    #  slice(dd,4:6) == OrderedDict{Symbol,String}(:Drn => "dragon",
-    #      		                                :Exp => "expensive",
-    #      		                                :Frg => "forage")
+    od = OrderedDict(
+             :Apl => "apple",
+     		 :Brc => "birch",
+     		 :Cnd => "candle",
+     		 :Drn => "dragon",
+     		 :Exp => "expensive",
+     		 :Frg => "forage",
+     		 :Gra => "grain",
+     		 :Hlt => "health",
+     		 :Irn => "irony",
+     		 :Jak => "jackal" 
+    )
+    @test sliced(od,4:6) == OrderedDict{Symbol,String}( :Drn => "dragon",
+          		                                        :Exp => "expensive",
+          		                                        :Frg => "forage")
 end
