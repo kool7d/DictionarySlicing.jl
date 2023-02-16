5c1ea0968d7c39efa329e09ab68bdd03e854dc62
kool7d <kool7d@gmail.com>
2023-02-15 20:44:11 -0600

    add string and symbol index

--- README.md
+++ README.md
@@ -22,7 +22,7 @@ dxs = sliced(od, 4)
 	OrderedDict{Symbol, String} with 1 entry:
 	  :Drn => "dragon"
 
-dxs = sliced(od, 2, 4:5, [7,8], 5:length(od), [8,5])  # instead of `end` we get the last index with `length`
+dxs = sliced(od, 2, 4:5, [7,8], 5:length(od), [8,5])  # instead of `end`, get the last index with `length`
 
 	OrderedDict{Symbol, String} with 8 entries:
 	  :Brc => "birch"           		 #  2
@@ -63,6 +63,23 @@ dxs = sliced(od, 2, 4:5, [7,8], 5:length(od), [8,5]; keep = :last, filter = x->i
 	  :Exp => "expensive"		 	 #  5
 ```
 Ordinarily `keep = :last` occurs after filtering, but if for some reason you want it to happen before the filter use `keep = :lastbefore`. Likewise, if you want it to keep the first instance before filtering, do `keep = :firstbefore`. This is possibly desirable if your filter function involves the number of occurrences.
+
+Since we are working with dictionaries, we are often dealing with keys that are Symbols or Strings. These keys also work, instead of or in combination with numbers.
+
+```julia
+dxs = sliced(od, 2, 4:5, [7,8], 5:length(od), [8,5], :Apl) 
+
+	OrderedDict{Symbol, String} with 8 entries:
+	  :Brc => "birch"           		 #  2
+	  :Drn => "dragon"			 #  4
+	  :Exp => "expensive"		 	 #  5
+	  :Gra => "grain"  			 #  7
+	  :Hlt => "health"			 #  8
+	  :Frg => "forage"			 #  6
+	  :Irn => "irony"			 #  9
+	  :Jak => "jackal"			 #  10
+	  :Apl => "apple"			 #  1
+```
 
 
 The function syntax version only works with OrderedDict, so convert your dict to an OrderedDict to use this.
--- src/DictionarySlicing.jl
+++ src/DictionarySlicing.jl
@@ -1,22 +1,40 @@
 module DictionarySlicing
 
-using OrderedCollections
+using Reexport
+@reexport using OrderedCollections
 
-export collectall, sliced, directslicing
+export collectall, sliced, directslicing, reversekv, printkv
+
+function collectkeys(args)
+    return keys(args) |> collect
+end
+
+function collectvals(args)
+    return values(args) |> collect
+end
 
 """
     collectall(sets...; maxdepth=5)
 
 Recursively collects elements from `sets`.
 
-Keyword arguments:
-maxdepth --------- determines maximum recursion level
-currentdepth ----- current recursion level
+### Keyword Arguments:
+- maxdepth = 5 --------- determines maximum recursion level
+- currentdepth = 1 ----- current recursion level
 """
 function collectall(sets...; maxdepth = 5, currentdepth = 1)
 	ff = []
 	f1 = collect([sets...])
-	f2 = collect([Base.Iterators.flatten(f1)...])
+	f2 = []
+	for x in f1
+		if typeof(x) <: Union{AbstractString,Symbol}
+			push!(f2,x)
+		else
+			for y in collect([Base.Iterators.flatten(x)...])
+				push!(f2,y)
+			end
+		end
+	end
 
     for x in f2
 		if typeof(x) <: Union{AbstractArray,AbstractRange} && currentdepth < maxdepth
@@ -31,103 +49,203 @@ function collectall(sets...; maxdepth = 5, currentdepth = 1)
 		end
 	end
     
-	typ = typeof(ff[1])
-    try
-        ff = Vector{typ}(ff)
-    catch
-
-    end
-    
-	return ff
+	return identity.(ff)
 end
 
 """
-    sliced(D::T, idxs...; keep = :first, filter = nothing) where {T<:AbstractDict}
+    sliced(D::T, idxs...; keep = :first, filter = nothing, maxdepth = 5) where {T<:AbstractDict}
 
 Returns the slices from dict `D`.
 
-Keyword args:
-keep --------- either keep the :first or :last instance of a sliced
-filter ------- true/false-returning function to apply to collected indices
+### Keyword Arguments:
+- keep = :first ---------- either keep the :first or :last instance of a key
+- filter = nothing ------- true/false-returning function to apply to collected inputs
+- maxdepth = 5 ----------- determines maximum recursion level for collection of idxs
 """
-function sliced(D::T, idxs...; keep = :first, filter = nothing) where {T<:AbstractDict}
-	inputs = collectall(idxs...)
-	_keys = []
-	_indices = []
-	for i in 1:size(inputs,1)
-		if typeof(inputs[i])<:AbstractString
-			push!(_keys,(i,inputs[i]))
-		else
-			push!(_indices,(i,inputs[i]))
-		end
-	end
+function sliced(D::T, idxs...; keep = :first, filter = nothing, maxdepth = 5) where {T<:AbstractDict}
+	inputs = collectall(idxs...; maxdepth = 5)
 	
 	if filter != nothing
 		try
 			if keep == :lastbefore || keep == "lastbefore"
-				revindices = reverse(indices)
+				inputs = unique(inputs)
+				revindices = reverse(inputs)
 				revindices = Base.filter(filter,revindices)
-				od = OrderedDict([([keys(D)...][i],[values(D)...][i]) for i in revindices])
+				kvs = []
+				ks = collectkeys(D)
+				vs = collectvals(D)
+				for inp in revindices
+					if typeof(inp)<:Int
+						push!(kvs,Pair(ks[inp],vs[inp]))
+					else
+						try
+							push!(kvs,Pair(inp,D[inp]))
+						catch
+							inp = string(inp)
+							push!(kvs,Pair(inp,D[inp]))
+						end
+					end
+				end
+				od = OrderedDict(kvs)
 				rv = length(od):-1:1
 				return sliced(od,rv...)
 			elseif keep == :firstbefore || keep == "firstbefore"
-				indices = unique(indices)
-				return sliced(D,indices...; filter = filter)
+				inputs = unique(inputs)
+				return sliced(D,inputs...; filter = filter)
 			else
-				indices = Base.filter(filter,indices)
+				inputs = Base.filter(filter,inputs)
 			end
 		catch
 			error("could not filter")
 		end
 	end
 	if keep == :last || keep == "last"
-		revindices = reverse(indices)
-		od = OrderedDict([([keys(D)...][i],[values(D)...][i]) for i in revindices])
+		revindices = reverse(inputs)
+		kvs = []
+		ks = collectkeys(D)
+		vs = collectvals(D)
+		for inp in revindices
+			if typeof(inp)<:Int
+				push!(kvs,Pair(ks[inp],vs[inp]))
+			else
+				try
+					push!(kvs,Pair(inp,D[inp]))
+				catch
+					inp = string(inp)
+					push!(kvs,Pair(inp,D[inp]))
+				end
+			end
+		end
+		od = OrderedDict(kvs)
 		rv = length(od):-1:1
 		return sliced(od,rv...)
 	end
-	ks = [[keys(D)...][i] for i in indices]
-	vs = [[values(D)...][i] for i in indices]
+	kvs = []
+	ks = collectkeys(D)
+	vs = collectvals(D)
+	for inp in inputs
+		if typeof(inp)<:Int
+			push!(kvs,Pair(ks[inp],vs[inp]))
+		else
+			try
+				push!(kvs,Pair(inp,D[inp]))
+			catch
+				inp = string(inp)
+				push!(kvs,Pair(inp,D[inp]))
+			end
+		end
+	end
 
-    return T(ks.=>vs)
+    return OrderedDict(kvs)
 end
+
 """
     directslicing()
 
 Activates indexing/slicing for OrderedDict with function indexing.
 `dict(2,3)` returns an OrderedDict of the 2nd and 3rd entry.
+
+### Keyword Arguments:
+- keep = :first ---------- either keep the :first or :last instance of a key
+- filter = nothing ------- true/false-returning function to apply to collected inputs
+- maxdepth = 5 ----------- determines maximum recursion level for collection of idxs
 """
 function directslicing()
-	@eval(function (D::OrderedDict)(idxs...; keep = :first, filter = nothing)
-		indices = collectall(idxs...)
+	@eval(function (D::OrderedDict)(idxs...; keep = :first, filter = nothing, maxdepth = 5)
+		inputs = collectall(idxs...; maxdepth = 5)
 
 		if filter != nothing
 			try
 				if keep == :lastbefore || keep == "lastbefore"
-					revindices = reverse(indices)
+					inputs = unique(inputs)
+					revindices = reverse(inputs)
 					revindices = Base.filter(filter,revindices)
-					od = OrderedDict([([keys(D)...][i],[values(D)...][i]) for i in revindices])
+					kvs = []
+					ks = collectkeys(D)
+					vs = collectvals(D)
+					for inp in revindices
+						if typeof(inp)<:Int
+							push!(kvs,Pair(ks[inp],vs[inp]))
+						else
+							try
+								push!(kvs,Pair(inp,D[inp]))
+							catch
+								inp = string(inp)
+								push!(kvs,Pair(inp,D[inp]))
+							end
+						end
+					end
+					od = OrderedDict(kvs)
 					rv = length(od):-1:1
 					return od(rv...)
 				elseif keep == :firstbefore || keep == "firstbefore"
-					indices = unique(indices)
-					return D(indices...; filter = filter)
+					inputs = unique(inputs)
+					return D(inputs...; filter = filter)
 				else
-					indices = Base.filter(filter,indices)
+					inputs = Base.filter(filter,inputs)
 				end
 			catch
 				error("could not filter")
 			end
 		end
 		if keep == :last || keep == "last"
-			revindices = reverse(indices)
-			od = OrderedDict([([keys(D)...][i],[values(D)...][i]) for i in revindices])
+			revindices = reverse(inputs)
+			kvs = []
+			ks = collectkeys(D)
+			vs = collectvals(D)
+			for inp in revindices
+				if typeof(inp)<:Int
+					push!(kvs,Pair(ks[inp],vs[inp]))
+				else
+					try
+						push!(kvs,Pair(inp,D[inp]))
+					catch
+						inp = string(inp)
+						push!(kvs,Pair(inp,D[inp]))
+					end
+				end
+			end
+			od = OrderedDict(kvs)
 			rv = length(od):-1:1
 			return od(rv...)
 		end
+		kvs = []
+		ks = collectkeys(D)
+		vs = collectvals(D)
+		for inp in inputs
+			if typeof(inp)<:Int
+				push!(kvs,Pair(ks[inp],vs[inp]))
+			else
+				try
+					push!(kvs,Pair(inp,D[inp]))
+				catch
+					inp = string(inp)
+					push!(kvs,Pair(inp,D[inp]))
+				end
+			end
+		end
 
-	    return OrderedDict([([keys(D)...][i],[values(D)...][i]) for i in indices])
+	    return OrderedDict(kvs)
 	end)
 end
+
+function reversekv(dict::AbstractDict{K,V}) where {K,V}
+	vkdict = [x[2].=>x[1] for x in dict]
+    if typeof(dict) <: OrderedDict
+        return OrderedDict{V,K}(vkdict)
+    end
+	return Dict{V,K}(vkdict)
+end
+
+function printkv(dict::AbstractDict)
+	keys1 = collectkeys(dict)
+	vals1 = collectvals(dict)
+	println.(["$(keys1[i]) -> $(vals1[i])" for i in 1:size(keys1,1)])
+	return nothing
+end
+
+typefields(thing) = typeof(thing) |> fieldnames
+
+tyf(thing) = typefields(thing)
 
 end
